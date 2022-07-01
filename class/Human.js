001569c66121e809140d9cfa47a43be4357e8cc8
Marius <marius@example.com>
2022-07-01 08:54:28 +0300

    Human class

--- class/Human.js
+++ class/Human.js
@@ -1,7 +1,14 @@
 // sukurti klase Human
-
-// klase ima amziu ir varda kaip argumentus
-
-// viduje yra metodas kuris apskaiciuoja gimimo metus su vardus (log)
-
-export const test = 5;
+export default class Human {
+  // klase ima amziu ir varda kaip argumentus
+  constructor(argAge, argName) {
+    this.age = argAge;
+    this.name = argName;
+  }
+  // viduje yra metodas kuris apskaiciuoja gimimo metus su vardus (log)
+  calculateYearOfBirth() {
+    const thisYear = new Date().getFullYear();
+    const yearOfBirth = thisYear - this.age;
+    console.log(`${this.name} was born on ${yearOfBirth}`);
+  }
+}
